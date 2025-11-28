ff54109b701279f00127e70c8ca0dd081b82d997
Leon Lam <leonlam34@gmail.com>
2025-11-28 00:12:33 -0600

    trying to push config

--- lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      -- Defer to the config module; use pcall to avoid throwing if not installed yet
-    end,
-  },
-}
